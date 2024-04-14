b1a5155b3c5cfd9dc14c5229b067952c7ef72010
zheyu <146802142+czheyu@users.noreply.github.com>
2024-04-14 03:44:31 +0000

    tryna get it working

--- SIL-code/Backend_Server/index.js
+++ SIL-code/Backend_Server/index.js
@@ -3,31 +3,42 @@ const express = require("express");
 
 const app = express();
 const port = 5000;
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/messages", (req, res) => {
   res.send(readData());
 });
 
-app.post("/api/messages", (req, res) => {
-  res.send(writeData(req.body));
+app.post("/api/messagesend", function (req, res) {
+  console.log(req.body);
+  writeData(req.body.type, req.body.username, req.body.value);
+  res.send(readData());
 });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 function readData() {
-    let dataarray = require("./data.json");
-    return dataarray;
+  let dataarray = require("./data.json");
+  return dataarray;
 }
 
-function writeData(data) {
+function writeData(type, username, value) {
   try {
-    let dataarray = require("./data.json");
-    dataarray.data.push(data);
+    let dataarray = JSON.parse(require("./data.json"));
+    let data = {
+      id: dataarray.countaccess,
+      username: username,
+      type: type,
+      value: value,
+    };
+    dataarray["data"].push(data);
     dataarray.countaccess++;
-    fs.writeFileSync("./data.json", JSON.stringify(dataarray));
+    fs.writeFile("./data.json", JSON.stringify(dataarray));
     return "success";
-  } catch {
+  } catch (as) {
+    console.log(as);
     return "error";
   }
 }
