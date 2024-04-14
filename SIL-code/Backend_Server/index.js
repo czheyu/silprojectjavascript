eef120ce9da66432ab4259ab62c55ccee93a49c8
zheyu <146802142+czheyu@users.noreply.github.com>
2024-04-14 07:57:48 +0000

    sending file works

--- SIL-code/Backend_Server/index.js
+++ SIL-code/Backend_Server/index.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const port = 5000;
+const path = require("path");
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(__dirname+ "/index.html");
 });
 app.get("/api/messagesget", (req, res) => {
   res.send(require("./data.json")); //need
