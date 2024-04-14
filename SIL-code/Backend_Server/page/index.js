54f6ac14491b7823046dee05d5f5e7ede35a646f
zheyu <146802142+czheyu@users.noreply.github.com>
2024-04-14 09:33:08 +0000

    chat app display finished
    das crazy

--- SIL-code/Backend_Server/page/index.js
+++ SIL-code/Backend_Server/page/index.js
@@ -37,12 +37,60 @@ function sendClicked() {
 }
 
 function updateChat(data) {
-  console.log(data);
+  let formatted = format(data);
+  const chatContainer = document.getElementById("chat-container");
+  //const chatMessage = document.createElement("div");
+  //chatMessage.classList.add("chat-message");
+  //chatMessage.innerHTML = data;
+  //chatContainer.appendChild(chatMessage);
+  chatContainer.innerHTML = formatted;
 }
+
+//https://javascript.plainenglish.io/how-to-really-implement-the-sleep-function-in-javascript-621b4ed1e618
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getCycle() {
+  console.log("getting data...");
+  getdata();
+  await sleep(2500);
+  getCycle();
+}
+
+function getdata() {
+  const apiUrl =
+    "https://9f385a7a-d4b2-4c35-b8fc-9937e0c39c58-00-zrl36mrg5918.picard.replit.dev/api/messagesget";
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      updateChat(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function format(data){
+  //data is a {}
+  let formated = "";
+  for (let i = 0; i < data.data.length; i++){
+    formated += "<div class='chat-message'>" + data.data[i].username + ": " + data.data[i].value + "</div>\n";
+  }
+  return formated;
+}
+
 window.onload = function () {
   console.log("page loaded.");
   const sendbutton = document.getElementById("sendbutton");
   sendbutton.onclick = function () {
     sendClicked();
   };
+  //start get cycle
+  getCycle();
 };
