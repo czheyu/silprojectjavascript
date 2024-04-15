36e30d0550a33530317c371a345ada8897cfb371
zheyu <146802142+czheyu@users.noreply.github.com>
2024-04-15 07:18:51 +0000

    port fix?

--- SIL-code/Chat_App/index.js
+++ SIL-code/Chat_App/index.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
-const port = 443;
+const port = process.env.PORT || 3001;
+
 const path = require("path");
 app.use(express.json());
 
