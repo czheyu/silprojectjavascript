9988d50346b6bb5f3184cef710bf4e94b23e0759
zheyu <146802142+czheyu@users.noreply.github.com>
2024-04-16 02:39:46 +0000

    changed links for render

--- SIL-code/Chat_App/registerpage/index.js
+++ SIL-code/Chat_App/registerpage/index.js
@@ -1,6 +1,6 @@
 var url = "https://czheyuchatapp.onrender.com";
 
-url = "https://9f385a7a-d4b2-4c35-b8fc-9937e0c39c58-00-zrl36mrg5918.picard.replit.dev:3001"
+//url = "https://9f385a7a-d4b2-4c35-b8fc-9937e0c39c58-00-zrl36mrg5918.picard.replit.dev:3001"
 
 
 
