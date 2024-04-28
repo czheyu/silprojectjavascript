77a57f30e6c2f8604d2eb93bc71e7b736477d847
zheyu <146802142+czheyu@users.noreply.github.com>
2024-04-28 09:53:30 +0000

    removed notif

--- SIL-code/Website/chatpage/index.js
+++ SIL-code/Website/chatpage/index.js
@@ -388,32 +388,6 @@ function getdata() {
         updateChat(data);
         
 
-        if (!document.hasFocus()&&getlastmsg(data.data)!=lastnotifyablemsg){
-          if (!("Notification" in window)) {
-             // Check if the browser supports notifications
-             alert("This browser does not support desktop notification");
-           } else if (Notification.permission === "granted") {
-             // Check whether notification permissions have already been granted;
-             // if so, create a notification
-             
-             const notification = new Notification("New Message",{
-               body: `${lastnotifyablemsg.username}: ${lastnotifyablemsg.value}`
-             });
-             // …
-           } else if (Notification.permission !== "denied") {
-             // We need to ask the user for permission
-             Notification.requestPermission().then((permission) => {
-               // If the user accepts, let's create a notification
-               if (permission === "granted") {
-
-                  const notification = new Notification("New Message",{
-                    body: `${lastnotifyablemsg.username}: ${lastnotifyablemsg.value}`
-                  });
-                 // …
-               }
-             });
-           }
-         }
       } else {
         //console.log("data is the same, did not redisplay.");
       }
