c65e06b36f00db8ab4954a978875f0d580ba458c
zheyu <146802142+czheyu@users.noreply.github.com>
2024-05-17 13:58:51 +0000

    actually put in the right endpoint

--- SIL-code/Website/chatlist/index.js
+++ SIL-code/Website/chatlist/index.js
@@ -13,7 +13,7 @@ function createchatfunct(){
 
 function deleteacc(){
   if(confirm("Are you sure you want to delete your account?")){
-    fetch(url + "/chat/deleteacc", {
+    fetch(url + "/api/removeuser {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
