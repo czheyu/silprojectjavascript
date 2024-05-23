58d02a79ee830c34f9edc318da452ff68f3fd6bd
zheyu <146802142+czheyu@users.noreply.github.com>
2024-05-23 08:31:29 +0000

    revamped ui
    displaying users

--- SIL-code/Website/chatpage/index.js
+++ SIL-code/Website/chatpage/index.js
@@ -9,6 +9,10 @@ var url = "https://czheyuchatapp.onrender.com";
 var replying = false;
 var showdateunhover = false;
 var replyingtoID;
+var htmlsvguser = `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="60" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+</svg>`
+
 //var chatID;
 
 function getlastmsg(data){
@@ -217,9 +221,14 @@ async function getparticipants(){
     })
     .then((data) => {
       if(data!=lastparticipants){
+        
         lastparticipants = data;
         const participantsdisplay = document.getElementById('participantsdisplay');
         participantsdisplay.innerHTML = "Participants: <strong>" + data.result + "</strong>"
+
+        const participantsmaindisplay = document.getElementById("participantsmaindisplay");
+        participantsmaindisplay.innerHTML = `${lastparticipants}`
+        
         displaytodropdownremove(data.result);
       }
       return data.result;
@@ -803,9 +812,15 @@ function getchatnamebyid(id){
       }
     }).then((data) => {
       const chatnamedisplay = document.getElementById("chatnamedisplay");
-      chatnamedisplay.innerHTML = `Chat: <button type="button" class="btn btn-outline-light rounded" data-bs-toggle="modal" data-bs-target="#chat"><strong>${data.result}</strong></button>`;
+
+      
+      const participantsmaindisplay = document.getElementById("participantsmaindisplay");
+      participantsmaindisplay.innerHTML = `${lastparticipants}`
+
+      
+      chatnamedisplay.innerHTML = `<strong>${data.result}</strong><br>`;
       const chatmenutitle = document.getElementById("chatmenutitle");
-      chatmenutitle.innerHTML = `Chat: <strong>${data.result}</strong>`;
+      chatmenutitle.innerHTML = `<strong>${data.result}</strong>`;
     });
   return;
 }
@@ -824,7 +839,7 @@ async function setEvents(){
   logindisplay.innerHTML = "Logged in as <strong>" + username + "</strong>";
 
   const userdisplay = document.getElementById("userdisplay");
-  userdisplay.innerHTML = "<strong>" + username + "</strong>";
+  userdisplay.innerHTML = "<strong>" + username + "</strong>" + htmlsvguser;
 
   let logout = document.getElementById("logoutbutton");
   logout.onclick = function () {
